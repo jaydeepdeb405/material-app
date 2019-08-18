2b3b10a3c2ff002bd9534364d3232a78523cfc94
Jaydeep <jaydeep.jaydeep.deb@gmail.com>
2019-08-19 01:25:54 +0530

    added promise to get auth2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,25 +9,35 @@ declare var gapi: any;
 })
 export class LoginComponent implements OnInit {
 
+  auth2: any;
+
   constructor() { }
 
   ngOnInit() {
-    gapi.load('auth2', function() {
-      gapi.auth2.init({
-        client_id: '946880795736-5ojeo39e17n91ggfena8unt59bfnc0l7.apps.googleusercontent.com'
-      }).then((authInstance) => {
-        console.log(authInstance);
-      })
-    });  
-    // if (auth2.isSignedIn.get()) {
-    //   var profile = auth2.currentUser.get().getBasicProfile();
-    //   console.log('ID: ' + profile.getId());
-    //   console.log('Full Name: ' + profile.getName());
-    //   console.log('Given Name: ' + profile.getGivenName());
-    //   console.log('Family Name: ' + profile.getFamilyName());
-    //   console.log('Image URL: ' + profile.getImageUrl());
-    //   console.log('Email: ' + profile.getEmail());
-    // }
+    this.loadAuth2().then(() => {this.displayLoginInfo()});
+  }
+
+  loadAuth2() {
+    return new Promise((resolve, reject) => {
+      gapi.load('auth2', function() {
+        gapi.auth2.init({
+          client_id: '946880795736-5ojeo39e17n91ggfena8unt59bfnc0l7.apps.googleusercontent.com'
+        }).then(()=>resolve('success'))
+      });   
+    });
+  }
+
+  displayLoginInfo() {
+    let auth2 = gapi.auth2.getAuthInstance();
+    if (auth2.isSignedIn.get()) {
+      var profile = auth2.currentUser.get().getBasicProfile();
+      console.log('ID: ' + profile.getId());
+      console.log('Full Name: ' + profile.getName());
+      console.log('Given Name: ' + profile.getGivenName());
+      console.log('Family Name: ' + profile.getFamilyName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail());
+    }
   }
 
 }
