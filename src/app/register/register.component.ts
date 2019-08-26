1159f69c6ba69811abe9b1a7e9c483afd9d21eb0
Jaydeep <jaydeep.jaydeep.deb@gmail.com>
2019-08-27 05:11:36 +0530

    Created register form layout

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  registerForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl(''),
+    confirmPassword: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  register() {
+    console.log(this.registerForm.value)
   }
 
 }
